71af9e02f0cc2cfa6ad03f3aa15ff429c76e0706
Ashwin Wilson <114730487+Ashwin-Wilson@users.noreply.github.com>
2025-07-26 14:25:31 +0530

    flappy bird game with key controls added

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 shopping_lt = []
+
 while True:
-    item = input(f"Shopping list: {shopping_lt}\n(press q to exit)\nEnter item: ")
+    item = input(f"\nShopping list: {shopping_lt}\nPress q to exit\nEnter item to add: ")
     if item == 'q':
         break
     shopping_lt.append(item)
+print("List insertion mode exited!\n\n")
 
 while True:
-    item = input(f"Shopping list: {shopping_lt}\n(press q to exit)\nEnter the item to remove: ")
+    item = input(f"\nShopping list: {shopping_lt}\nPress q to exit\nEnter item to remove: ")
     if item == 'q':
         break
+    if len(shopping_lt) == 0:
+        print("List is empty\n")
+        break
+    if item not in shopping_lt:
+        print("Item not in the list\n\n")
+        continue
     shopping_lt.remove(item)
-print(shopping_lt)
-    
