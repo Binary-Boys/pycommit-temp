5f77826c294d2e3f3e9597d618ea800cb20c2d98
Ashwin Wilson <114730487+Ashwin-Wilson@users.noreply.github.com>
2025-07-26 16:30:37 +0530

    This is a commit

--- test.py
+++ test.py
@@ -1,20 +1,30 @@
-shopping_lt = []
+import pygame
 
-while True:
-    item = input(f"\nShopping list: {shopping_lt}\nPress q to exit\nEnter item to add: ")
-    if item == 'q':
-        break
-    shopping_lt.append(item)
-print("List insertion mode exited!\n\n")
+pygame.init()
 
-while True:
-    item = input(f"\nShopping list: {shopping_lt}\nPress q to exit\nEnter item to remove: ")
-    if item == 'q':
-        break
-    if len(shopping_lt) == 0:
-        print("List is empty\n")
-        break
-    if item not in shopping_lt:
-        print("Item not in the list\n\n")
-        continue
-    shopping_lt.remove(item)
+#SCREEN SIZE    
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+img = pygame.image.load("images/bird.png")
+
+clock = pygame.time.Clock()
+
+run = True
+x = 0
+
+while run:
+    screen.fill((0, 0, 0))
+  
+    screen.blit(img, (x, 30))
+    x += 1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    clock.tick(60)
+    pygame.display.flip()
+pygame.quit()
+
+
